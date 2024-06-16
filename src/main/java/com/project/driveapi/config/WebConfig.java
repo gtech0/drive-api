1d9927197ab18259fe6cc58088b87ea4b62abbb4
gtech <dimadim202@gmail.com>
2024-06-17 04:13:51 +0700

    expose headers

---
+++ src/main/java/com/project/driveapi/config/WebConfig.java
@@ -0,0 +1,34 @@
+package com.project.driveapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class WebConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .cors(cors -> cors.configurationSource(_ -> {
+                    CorsConfiguration configuration = new CorsConfiguration();
+                    configuration.setAllowedOrigins(List.of("*"));
+                    configuration.setAllowedMethods(List.of("*"));
+                    configuration.setAllowedHeaders(List.of("*"));
+                    configuration.setExposedHeaders(List.of("Content-Disposition"));
+                    return configuration;
+                }))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
--- src/main/java/com/project/driveapi/exception/ControllerErrorHandler.java
+++ src/main/java/com/project/driveapi/exception/ControllerErrorHandler.java
@@ -2,6 +2,7 @@ package com.project.driveapi.exception;
 
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.project.driveapi.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Date;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerErrorHandler {
 
@@ -23,48 +25,56 @@ public class ControllerErrorHandler {
         for (FieldError fieldError : ex.getBindingResult().getFieldErrors()) {
             errorResponse.addValidationError(new Date(), fieldError.getField(), fieldError.getDefaultMessage());
         }
+        log.error(errorResponse.getMessage());
         return ResponseEntity.unprocessableEntity().body(errorResponse);
     }
 
     @ExceptionHandler(GoogleJsonResponseException.class)
     public ResponseEntity<Object> handleGoogleJsonResponseException(GoogleJsonResponseException ex) {
         ErrorResponse errorResponse = new ErrorResponse(new Date(), ex.getStatusCode(), ex.getDetails().getMessage());
+        log.error(errorResponse.getMessage());
         return ResponseEntity.status(ex.getStatusCode()).body(errorResponse);
     }
 
     @ExceptionHandler(IOException.class)
     public ResponseEntity<Object> handleIOException(IOException ex) {
         ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        log.error(errorResponse.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(errorResponse);
     }
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<Object> handleSQLException(SQLException ex) {
         ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+        log.error(errorResponse.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(errorResponse);
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
         ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.NOT_FOUND.value(), ex.getMessage());
+        log.error(errorResponse.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(errorResponse);
     }
 
     @ExceptionHandler(DuplicateFileException.class)
     public ResponseEntity<Object> handleDuplicateFileException(DuplicateFileException ex) {
         ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.CONFLICT.value(), ex.getMessage());
+        log.error(errorResponse.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT.value()).body(errorResponse);
     }
 
     @ExceptionHandler(FileNotExportableException.class)
     public ResponseEntity<Object> handleFileNotExportableException(FileNotExportableException ex) {
         ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.CONFLICT.value(), ex.getMessage());
+        log.error(errorResponse.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT.value()).body(errorResponse);
     }
 
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<Object> handleUnknownException(Exception ex) {
-//        ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
-//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(errorResponse);
-//    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleUnknownException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
+        log.error(errorResponse.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(errorResponse);
+    }
 }
